5cdaf8ff426f45889b5a314f15c3ed478c4719d9
Nissrine Canina <niss.canina@gmail.com>
2021-09-17 10:49:10 -0400

    Complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+let form = document.querySelector('form')
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    createTodo(e.target.querySelector('#new-task-description').value);
+    form.reset()
+  });
+
+  function createTodo(task) {
+    
+    let li = document.createElement('li')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+
+    btn.textContent = 'x' 
+    li.textContent = `${task} `
+    li.appendChild(btn)
+    document.querySelector('#tasks').appendChild(li)
+  }
+
+  function handleDelete(e) {
+    e.target.parentNode.remove()
+  }
+
+
+
